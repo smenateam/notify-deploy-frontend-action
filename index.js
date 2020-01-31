9091344f98f972ac0d6b4291574805f92fa0a618
salmasov <ilya.salmasov@gmail.com>
2020-01-31 15:35:26 +0500

    с бохам

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const Youtrack = require('youtrack-rest-client')
+const fetch = require('node-fetch')
+const rp = require('request-promise')
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    // переменные
+    const youtrack_token = core.getInput('youtrack_token', { required: true })
+    const repo_token = core.getInput('repo_token', { required: true })
+    const youtrack_url = core.getInput('youtrack_url', { required: true })
+    const base_url = core.getInput('base_url', { required: true })
+    const login = core.getInput('basic_login', { required: true })
+    const password = core.getInput('basic_password', { required: true })
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    // из пуллреквеста
+    const pr_title = github.context.payload.pull_request['title']
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+    // получаем  taskname из pr_title
+    const regex = new RegExp(/^([A-Z]+-\d+)/)
+    const taskname = pr_title.match(regex)[1]
+
+    // Выгружено на http://ufa.site-368.frfrsite.ru/ и http://bratsk.site-368.frfrsite.ru/
+    const remote_url = `http://ufa.${taskname}.${base_url}`
+
+    // проверяем что ветка выгрузилась
+    await rp(remote_url, {
+      auth: {
+        user: login,
+        pass: password,
+        sendImmediately: false,
+      },
+    }).catch(() => {
+      throw new Error(`По адресу ${remote_url} ошибка`)
+    })
+
+    // если ветка не выгрузилась - роняем action
+
+    const message = `Выгружено на ${remote_url}`
+
+    // отправляем на youtrack сообщение о статусе ветки
+    const issue = await youtrack.issues.byId(taskname)
+    const youtrack = new Youtrack.Youtrack({
+      baseUrl: youtrack_url,
+      token: youtrack_token,
+    })
+
+    youtrack.comments.create(issue.task_id, { text: message })
+
+    // отсылка на гитхаб
+    const client = new github.GitHub(repo_token)
+    await client.issues.createComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.payload.pull_request.number,
+      body: message,
+    })
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
